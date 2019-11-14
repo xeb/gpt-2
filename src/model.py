21f9ae96ae8e8fc8eff172f1a5a90829154a0f23
Shawn Presser <shawnpresser@gmail.com>
2019-11-13 17:14:53 -0800

    Set default vocab size to 50257 tokens

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ from tensorflow.contrib.training import HParams
 
 def default_hparams():
     return HParams(
-        n_vocab=0,
+        n_vocab=50257,
         n_ctx=1024,
         n_embd=768,
         n_head=12,
