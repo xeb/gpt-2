ac7bc9f48cadf028c39b50cc4ca43c3af8a56800
Shawn Presser <shawnpresser@gmail.com>
2019-11-15 13:00:06 -0600

    Minor watchdog fix

--- watchnonce.sh
+++ watchnonce.sh
@@ -1,2 +1,2 @@
 pid="$1"
-tail -n 100 pong.txt | egrep "pid${pid}_" | sort -n | tail -1 | egrep "time[0-9]+" -o | egrep "[0-9]+" -o
+cat pong.txt | egrep "pid${pid}_" | sort -n | tail -1 | egrep "time[0-9]+" -o | egrep "[0-9]+" -o
--- watchpids.sh
+++ watchpids.sh
@@ -1,1 +1,1 @@
-tail -n 100 pong.txt | egrep 'pid[0-9]+' -o | egrep '[0-9]+' -o | sort | uniq
+cat pong.txt | egrep 'pid[0-9]+' -o | egrep '[0-9]+' -o | sort | uniq
