c2e4188c05f01c0ff92d1a43321cc8399a801009
Shawn Presser <shawnpresser@gmail.com>
2019-11-26 21:41:43 -0600

    Also graph perplexity; average over 100 steps

--- train_multi.py
+++ train_multi.py
@@ -295,9 +295,8 @@ class TrainGPT2(object):
       self.avg_loss = (self.avg_loss[0] * 0.99 + v_loss,
                        self.avg_loss[1] * 0.99 + 1.0)
       v_perp = math.exp(v_loss)
-      if v_perp < 100:
-        self.avg_perp = (self.avg_perp[0] * 0.99 + v_perp,
-                         self.avg_perp[1] * 0.99 + 1.0)
+      self.avg_perp = (self.avg_perp[0] * 0.99 + v_perp,
+                       self.avg_perp[1] * 0.99 + 1.0)
       now = time.time()
       print('{stamp} [{counter} | {time:2.4f} | {delta:2.2f}s | {ops:2.6f}tokens/s] loss={loss:2.4f} avg={avg:2.4f} perp={perp:2.4f} avg_perp={avg_perp:2.4f} rate={rate:0.7f} step={step}'
           .format(
