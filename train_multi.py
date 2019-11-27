488427b1c6f768a1cad2edae09c74b4a66a88484
Shawn Presser <shawnpresser@gmail.com>
2019-11-26 21:38:56 -0600

    Also graph perplexity; average over 100 steps

--- train_multi.py
+++ train_multi.py
@@ -292,13 +292,14 @@ class TrainGPT2(object):
       self.context.load(batch, session=self.sess)
       self.say('Running opt_apply...')
       (_, v_loss, v_summary) = self.sess.run((self.opt_apply, self.loss, self.summaries), options=config_pb2.RunOptions(timeout_in_ms=self.timeout))
-      v_perp = math.exp(v_loss)
       self.avg_loss = (self.avg_loss[0] * 0.99 + v_loss,
                        self.avg_loss[1] * 0.99 + 1.0)
-      self.avg_perp = (self.avg_perp[0] * 0.99 + v_perp,
-                       self.avg_perp[1] * 0.99 + 1.0)
+      v_perp = math.exp(v_loss)
+      if v_perp < 100:
+        self.avg_perp = (self.avg_perp[0] * 0.99 + v_perp,
+                         self.avg_perp[1] * 0.99 + 1.0)
       now = time.time()
-      print('{stamp} [{counter} | {time:2.4f} | {delta:2.2f}s | {ops:2.6f}tokens/s] loss={loss:2.4f} avg={avg:2.4f} perp={loss:2.4f} avg_perp={avg_perp:2.4f} rate={rate:0.7f} step={step}'
+      print('{stamp} [{counter} | {time:2.4f} | {delta:2.2f}s | {ops:2.6f}tokens/s] loss={loss:2.4f} avg={avg:2.4f} perp={perp:2.4f} avg_perp={avg_perp:2.4f} rate={rate:0.7f} step={step}'
           .format(
               stamp=timestamp(),
               counter=self.counter,
