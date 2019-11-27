e7a1b90e29f6da258da584af15c65858952f9ba9
Shawn Presser <shawnpresser@gmail.com>
2019-11-26 21:14:16 -0600

    Also graph perplexity; average over 100 steps

--- train_multi.py
+++ train_multi.py
@@ -11,6 +11,7 @@ import numpy as np
 import tensorflow as tf
 import time
 import tqdm
+import math
 from tensorflow.core.protobuf import rewriter_config_pb2
 from tensorflow.core.protobuf import config_pb2
 from tensorflow.python import pywrap_tensorflow
