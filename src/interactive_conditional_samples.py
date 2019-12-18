0a4ae565e344f91d1619fba0a0002a614061131e
Shawn Presser <shawnpresser@gmail.com>
2019-12-18 11:57:44 -0600

    WIP poetry without the letter 'e'

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -13,10 +13,10 @@ def interact_model(
     seed=None,
     nsamples=1,
     batch_size=1,
-    length=None,
+    length=1,
     temperature=1,
     top_k=0,
-    top_p=0.0
+    top_p=0
 ):
     """
     Interactively run the model
@@ -72,18 +72,88 @@ def interact_model(
             while not raw_text:
                 print('Prompt should not be empty!')
                 raw_text = input("Model prompt >>> ")
+            raw_text = raw_text.replace('\\n', '\n')
+            print(repr(raw_text))
             context_tokens = enc.encode(raw_text)
             generated = 0
-            for _ in range(nsamples // batch_size):
-                out = sess.run(output, feed_dict={
-                    context: [context_tokens for _ in range(batch_size)]
-                })[:, len(context_tokens):]
-                for i in range(batch_size):
-                    generated += 1
-                    text = enc.decode(out[i])
-                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
+            for text in generate_poetry(prompt=raw_text, enc=enc, output=output, context=context, nsamples=1, batch_size=batch_size, sess=sess):
+                #print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
+                print(text)
+                generated += 1
             print("=" * 80)
+
+def generate_result(prompt, enc, output, context, nsamples=1, batch_size=1, sess=tf.get_default_session()):
+    context_tokens = enc.encode(prompt)
+    for _ in range(nsamples // batch_size):
+        out = sess.run(output, feed_dict={
+            context: [context_tokens for _ in range(batch_size)]
+        })[:, len(context_tokens):]
+        for i in range(batch_size):
+            text = enc.decode(out[i])
+            yield text
+
+def generate_line_no_e(prompt, **kws):
+    result = prompt
+    current = ''
+    while '\n' not in current:
+        for text in generate_result(prompt + current, **kws):
+            if 'e' not in text:
+                current += text
+    return prompt + current
+
+def generate_line_no_e(prompt, **kws):
+    result = prompt
+    current = ''
+    while '\n' not in current:
+        for text in generate_result(prompt + current, **kws):
+            if 'e' in text:
+                print(repr((prompt + current + text).splitlines()[-1]))
+                current = ''
+            else:
+                current += text
+    return prompt + current
+
+
+def generate_line(prompt, **kws):
+    result = prompt
+    current = ''
+    while '\n' not in current:
+        for text in generate_result(prompt + current, **kws):
+            current += text
+    return prompt + current
+
+import pronouncing as pro
+
+def last_word(text):
+  while len(text) > 0 and not str.isalnum(text[-1]):
+    text = text[0:-1]
+  return text.split()[-1]
+
+def generated_poem(prompt):
+  if prompt.endswith('\n'):
+    return prompt
+  else:
+    return '\n'.join(prompt.splitlines()[0:-1]) + '\n'
+
+def generate_poem(prompt, **kws):
+  rhymes = [x for x in pro.rhymes(last_word(generated_poem(prompt)))] #if 'e' not in x]
+  print(repr(rhymes))
+  while True:
+    attempt = generate_line(prompt, **kws)
+    print(repr(attempt.splitlines()[-1]))
+    if last_word(attempt) in rhymes:
+      return attempt
+
+def generate_poetry(prompt, **kws):
+  if len(generated_poem(prompt).rstrip().splitlines()) == 1 and not prompt.endswith('\n'):
+    prompt = generate_line(prompt, **kws)
+    print(repr(prompt))
+  if len(generated_poem(prompt).rstrip().splitlines()) % 2 == 0:
+    attempt = generate_line(prompt, **kws)
+    yield attempt
+  else:
+    text = generate_poem(prompt, **kws)
+    yield text
 
 if __name__ == '__main__':
     fire.Fire(interact_model)
