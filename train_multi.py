775106d0c1911839fb2f5cca542130c699197bc0
Shawn Presser <shawnpresser@gmail.com>
2019-12-31 11:01:10 -0600

    Add update_trainers_read_timeout and update_trainers_write_timeout

--- train_multi.py
+++ train_multi.py
@@ -1046,7 +1046,10 @@ def trainer_slice_write(trainer, accumulator, variables):
 
 tflex.trainer_slice_write = trainer_slice_write
 
-def update_trainers(trainers, i, sync_all=False, timeout=15):
+tflex.update_trainers_read_timeout = 60
+tflex.update_trainers_write_timeout = 60
+
+def update_trainers(trainers, i, sync_all=False):
   trainers = [x for x in trainers]
   if len(trainers) <= 0:
     return
@@ -1064,7 +1067,7 @@ def update_trainers(trainers, i, sync_all=False, timeout=15):
   start_time = time.time()
   for thread in threads:
     elapsed = (time.time() - start_time)
-    waiting = timeout - elapsed
+    waiting = tflex.update_trainers_read_timeout - elapsed
     if waiting > 0:
       thread.join(timeout=waiting)
   threads = []
@@ -1075,6 +1078,12 @@ def update_trainers(trainers, i, sync_all=False, timeout=15):
     thread = threading.Thread(target=thunk, args=(trainer,accumulator,i,))
     thread.start()
     threads.append(thread)
+  start_time = time.time()
+  for thread in threads:
+    elapsed = (time.time() - start_time)
+    waiting = tflex.update_trainers_write_timeout - elapsed
+    if waiting > 0:
+      thread.join(timeout=waiting)
 
 tflex.update_trainers = update_trainers
 
